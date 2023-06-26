c8ea85964d56b0ee800778b1e34e558313fffcff
the-soban <sobanshafeq123@gmail.com>
2023-06-26 19:27:30 +0500

    added functionality for opening and closing modal and displaying meal data in modal

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ searchBtn.addEventListener('click', () => {
 
 mealResults.addEventListener('click', getRecipe);
 
+modalCloseBtn.addEventListener('click', () => {
+    modalContent.parentElement.style.display = 'none';
+});
 
 function getMeals(){
     let searchInput = searchField.value.trim();
@@ -60,18 +63,19 @@ function getRecipe(e){
 function openModal(meal){
     meal = meal[0];
     let htmlData = `
-        <svg class="modal-close-btn" xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 -960 960 960" width="40"><path d="m251.333-204.667-46.666-46.666L433.334-480 204.667-708.667l46.666-46.666L480-526.666l228.667-228.667 46.666 46.666L526.666-480l228.667 228.667-46.666 46.666L480-433.334 251.333-204.667Z"/></svg>
-        <div class="modal-content">
-            <h3 class="modal-heading">Name Here</h3>
-            <p class="modal-category">Category Here</p>
+
+            <h3 class="modal-heading">${meal.strMeal}</h3>
+            <p class="modal-category">Category: ${meal.strCategory}</p>
             <div class="modal-instructions">
                 <h4 class="instructions-heading">Instructions</h4>
-                <p class="instructions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsa fugit nostrum dolorum illo itaque nemo voluptas omnis quisquam sequi asperiores ipsum veritatis enim consequuntur vel aspernatur, inventore, distinctio voluptatibus voluptate vitae alias, quia ipsam aperiam culpa. Aperiam nulla ea aliquam autem dicta similique voluptatem accusantium quis facere. Laboriosam a maiores quaerat aliquid illo quam ut, odio corrupti optio blanditiis rerum voluptates saepe tempore temporibus porro ratione velit, voluptas expedita.</p>
+                <p class="instructions">${meal.strInstructions}</p>
             </div>
             <div class="modal-image-box">
-                <img src="./images/food.jpg" alt="Meal Image" class="modal-image">
+                <img src="${meal.strMealThumb}" alt="Meal Image" class="modal-image">
             </div>
-            <a href="#" class="modal-link" target="_blank">Watch Video</a>
-        </div>
+            <a href="${meal.strYoutube}" class="modal-video" target="_blank">Watch Video</a>
+            <a href="${meal.strSource}" class="modal-source" target="_blank">Recipe Source</a>
     `;
+    modalContent.innerHTML = htmlData;
+    modalContent.parentElement.style.display = 'block';
 }
